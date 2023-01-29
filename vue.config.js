7257d9ccb12890aa8dd06b36108e8f51f81d97a4
stephaniedawson <stephanie.a.dawson@gmail.com>
2023-01-29 11:26:22 -0600

    create deploy

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+import { defineConfig } from '@vue/cli-service'
+export default defineConfig({
   transpileDependencies: true
 })
+
+export const publicPath = process.env.NODE_ENV === 'production'
+  ? '/book-app/'
+  : '/'
